a107025857ed5b9e47fc0ee53ffaa61aaf351f22
unknown <Vistark@NGHIA.tcu.edu.vn>
2019-11-15 00:46:22 +0700

    Upload source code

---
+++ app/build/generated/source/buildConfig/release/vistark/vn/rudant/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vistark.vn.rudant;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "vistark.vn.rudant";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 19111460;
+  public static final String VERSION_NAME = "19.11.14.60";
+}
